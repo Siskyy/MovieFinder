0281b1b3c6b9fccf3963fb4f1c3a0c58d278e09d
Siskyy <marcbertelli7@gmail.com>
2022-05-07 12:50:32 +0100

    fetch genres

--- public/script.js
+++ public/script.js
@@ -1,14 +1,29 @@
-const tmdbKey = '';
-const tmdbBaseUrl = '';
+const tmdbKey = '54e6c7d0807d204199521ac57e97ec9d';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
+const getGenres = async () => {
+    const genreRequestEndpoint = '/genre/movie/list';
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
+    try {
+        // send request
+        const response = await fetch(`${urlToFetch}`);
+        // handles response if successful
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const genres = jsonResponse;
+            console.log(genres);
+            return genres;
+        }
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 const getMovies = () => {
-  const selectedGenre = getSelectedGenre();
-
+    const selectedGenre = getSelectedGenre();
 };
 
 const getMovieInfo = () => {
